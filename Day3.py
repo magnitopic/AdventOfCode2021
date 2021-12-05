7ccd4b1a409ecdfde468ba602c8783ce47fde888
magnitopic <magnitopic@gmail.com>
2021-12-05 21:06:41 +0100

    Made Day3 a bit better

--- Day3.py
+++ Day3.py
@@ -4,24 +4,20 @@ pattern = random.random() < .5    # boolean
 limit = random.randint(3, 15)
 control = True
 
-
 def detect(array):
     for i in range(len(array)):
         if ((array[i] % 2 == 0) != ((array[i-1]) % 2 == 0)) and ((array[i] % 2 == 0) != ((array[i-2]) % 2 == 0)):
             return array[i]
 
-
 while len(array) < limit or control:
     number = random.randint(-99, 99)
-
-    if (number % 2 == 0 and pattern == True) or (number % 2 != 0 and pattern == False):
+    if (not(number % 2) and pattern == True) or (number % 2 and pattern == False):
         if len(array) < limit:
             array.append(number)
-
     elif control:
         control = False
         array.append(number)
 
-""" random.shuffle(array) """
+random.shuffle(array)
 print(array)
 print(f"The diferent number is {detect(array)}")
