478f8ed39bf000ce129fb11b2f3be6224e1d0cc0
magnitopic <magnitopic@gmail.com>
2021-12-09 15:57:50 +0100

    Reduced day 8 to 2 lines

--- Day8.py
+++ Day8.py
@@ -1,5 +1,4 @@
-x = int(input("Value for X: ") or 1)
-y = int(input("Value for Y: ") or 1)
-z = int(input("Value for Z: ") or 2)
-n = int(input("Value for N: ") or 3)
-print([[i,j,k] for i in range(x+1)for j in range(y+1)for k in range(z+1)if i+j+k != n])	#Generate an array in one line
+# Assign inputs to all 4 variables
+x,y,z,n = int(input("Value for X: ") or 1),int(input("Value for Y: ") or 1),int(input("Value for Z: ") or 2),int(input("Value for N: ") or 3)
+# Generate an array in one line
+print([[i,j,k] for i in range(x+1)for j in range(y+1)for k in range(z+1)if i+j+k != n])	
