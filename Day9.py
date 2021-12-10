5f81fce4fabe2bd3b4a0ba2ee8b74a3568f273a1
magnitopic <magnitopic@gmail.com>
2021-12-10 20:28:38 +0100

    More day9

--- Day9.py
+++ Day9.py
@@ -1,9 +1,12 @@
 import random
 n=random.randint(1,100)
-array=[]
-for i in range(n):
-	row=[]
-	for j in range(n):
-		row.append(random.randint(-9,9))
-	array.append(row)
+flag=True
+while flag:
+	array=[]
+	for i in range(n):
+		row=[]
+		for j in range(n):
+			row.append(random.randint(-9,9))
+		array.append(row)
+	flag = all(i ==0 for i in range(len(array)))
 print(array)
