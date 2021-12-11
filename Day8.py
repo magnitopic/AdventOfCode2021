17e9f1ecbf0d3d98bfc34e5c892f405a0803f2ec
magnitopic <magnitopic@gmail.com>
2021-12-11 21:18:44 +0100

    Best version so far day9

--- Day8.py
+++ Day8.py
@@ -1,4 +1,4 @@
 # Assign inputs to all 4 variables
 x,y,z,n = int(input("Value for X: ") or 1),int(input("Value for Y: ") or 1),int(input("Value for Z: ") or 2),int(input("Value for N: ") or 3)
 # Generate an array in one line
-print([[i,j,k] for i in range(x+1)for j in range(y+1)for k in range(z+1)if i+j+k != n])	
+print([[i,j,k] for i in range(x+1)for j in range(y+1)for k in range(z+1)if i+j+k != n])
--- Day9.py
+++ Day9.py
@@ -1,12 +1,20 @@
 import random
-n=random.randint(1,100)
-flag=True
-while flag:
-	array=[]
-	for i in range(n):
-		row=[]
-		for j in range(n):
-			row.append(random.randint(-9,9))
-		array.append(row)
-	flag = all(i ==0 for i in range(len(array)))
+import numpy
+# n=random.randint(1,5)
+n = 5
+flag1, flag2 = True, True
+while flag1:  # or flag2:
+    array = []
+    for i in range(n):
+        row = []
+        for j in range(n):
+            value = (numpy.random.choice(numpy.arange(0, 10),p=[.25, .2, .16, .12, .09, .07, .05, .03, .02, .01]))
+            if random.random() > .5:row.append(-value)
+            else:row.append(value)
+        array.append(row)
+    flag1 = all(j == 0 for i in range(n) for j in array[i])
+    #arrayVuelto=([[k] for i in range(n)for j in range(n)for k in range(n)])
+    arrayVuelto = [array[k][i] for i in range(n)for k in range(n)]
+    """ flag2 = all(j == 0 for i in range(n) for j in arrayVuelto[i]) """
+    print(arrayVuelto)
 print(array)
