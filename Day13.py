a5b3d8cc6ed47bf82e4b8542d75601f8ef0c7dc5
magnitopic <magnitopic@gmail.com>
2021-12-21 20:40:34 +0100

    Finished day16

--- Day13.py
+++ Day13.py
@@ -2,46 +2,38 @@ import numpy as np
 leters = ["A", "G", "C", "T"]
 
 
+# Function that creates a list of all groups of three letters that appear more than once
 def checkRepetitions():
     repetitions = []
-    for i in range(0, len(mutation), 3):
-        sequence = mutation[i:i+3]
-        if mutation.count(sequence) > 1 and len(sequence) == 3:
+    for i in range(len(mutation)):
+        sequence = mutation[i]
+        if mutation.count(sequence) > 1:
             repetitions.append(sequence)
     return repetitions
 
 
 # Create word
-word = ""
-for i in range(100):
-    # Probability indicated by the exercise
-    word += leters[np.random.choice(np.arange(0, 4), p=[.293, .207, .2, .3])]
-print("Original:", word)
+word = [''.join(leters[np.random.choice(np.arange(0, 4), p=[.293, .207, .2, .3])]for i in range(3))for i in range(34)]
 
+# Mutates the word
 mutation = word
 repetitions = checkRepetitions()
-print("Repetitions: ", repetitions)
-stop = False
-# Make mutations
 while len(repetitions) != 0:
-    print("repetitions:", repetitions)
     sequence = list(repetitions[0])
-    print(sequence)
-    for j in leters:
-        for i in range(len(leters)-1):
-            sequence[i] = j
-            # Use of ''.join() makes arrays strings of text
-            if repetitions.count(''.join(sequence)) == 0:
-                print("John was here")
-                print(mutation)
-                print(''.join(sequence))
-                mutation=mutation.replace(repetitions[0], ''.join(sequence),1)
-                repetitions = checkRepetitions()
+    for j in range(len(sequence)):
+        stop = False
+        for i in leters:
+            sequence[j] = i
+            if mutation.count(''.join(sequence)) == 0:
+                mutation = [w.replace(repetitions[0], ''.join(sequence), 1)for w in mutation]
+                repetitions.pop(0)
                 stop = True
                 break
         if stop:
             break
 
-
-# Binary register
-# (word[i]!=mutation[i] for i in range(len(word)))
+print("Original:", *word)
+print("Mutation:", *mutation)
+# Binary registry
+print("Binary registry", *[int(word[i] != mutation[i])
+      for i in range(len(word))])
