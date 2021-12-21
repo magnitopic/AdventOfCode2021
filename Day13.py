e425f26d9d1f2bf127279e2a40606201514b8384
magnitopic <magnitopic@gmail.com>
2021-12-21 15:56:16 +0100

    More advancements in day 13

--- Day13.py
+++ Day13.py
@@ -1,43 +1,46 @@
-from typing import Sequence
-import numpy
+import numpy as np
 leters = ["A", "G", "C", "T"]
 
 
 def checkRepetitions():
-    # Use of ''.join() makes arrays strings of text
     repetitions = []
-    for i in range(0, len(word), 3):
-        sequence = word[i:i+3]
-        if word.count(sequence) > 1 and len(sequence) == 3:
+    for i in range(0, len(mutation), 3):
+        sequence = mutation[i:i+3]
+        if mutation.count(sequence) > 1 and len(sequence) == 3:
             repetitions.append(sequence)
     return repetitions
 
-def seeRepetition(sequence):
-    return word.count(sequence)
 
 # Create word
 word = ""
 for i in range(100):
     # Probability indicated by the exercise
-    word +=leters[numpy.random.choice(numpy.arange(0, 4), p=[.293, .207, .2, .3])]
-print("Original:",word)
+    word += leters[np.random.choice(np.arange(0, 4), p=[.293, .207, .2, .3])]
+print("Original:", word)
 
 mutation = word
 repetitions = checkRepetitions()
-print(repetitions)
+print("Repetitions: ", repetitions)
+stop = False
 # Make mutations
-while repetitions != 0:
-    sequence=repetitions[0]
+while len(repetitions) != 0:
+    print("repetitions:", repetitions)
+    sequence = list(repetitions[0])
+    print(sequence)
     for j in leters:
-        for i in range(len(leters)):
-            if seeRepetition(sequence[j].replace(sequence[j],i,1))==0:
-                replacement=sequence[j].replace(sequence[j],i,1)
-                stop=True
+        for i in range(len(leters)-1):
+            sequence[i] = j
+            # Use of ''.join() makes arrays strings of text
+            if repetitions.count(''.join(sequence)) == 0:
+                print("John was here")
+                print(mutation)
+                print(''.join(sequence))
+                mutation=mutation.replace(repetitions[0], ''.join(sequence),1)
+                repetitions = checkRepetitions()
+                stop = True
                 break
         if stop:
             break
-    mutation.replace(sequence, "", 1)
-    repetitions = checkRepetitions()
 
 
 # Binary register
