b6bed350ebb9e1e8db88c4fb0ec1a07ae1020e8a
magnitopic <magnitopic@gmail.com>
2021-12-22 23:52:02 +0100

    improvements to day14

--- Day14.py
+++ Day14.py
@@ -1,28 +1,27 @@
-import numpy as np
-import random
+import random, string
 
-charecters = ["a", "b", "c", "d", "e", "f", "g", "h", "#", "@"]
+charecters = list(string.ascii_letters)
 
 list = []
 for i in range(20):
     rnd = random.random()
     if rnd < .45:
-        list.append(random.randint(0,100))
+        list.append(random.randint(0, 100))
     elif .45 < rnd and rnd < .80:
-        list.append(charecters[round(rnd*10)])
-    elif rnd > .80:
-        list.append(bool(round(rnd*10) % 2))
-print(list)
+        list.append(charecters[random.randint(0, len(charecters)-1)])
+    else:
+        list.append(bool(int(rnd*10) % 2))
+print(*list)
 
-ints,strs,bools=[],[],[]
+ints, strs, bools = [], [], []
 for i in list:
-	if isinstance(i, str):
-		strs.append(i)
-	elif isinstance(i, bool):
-		bools.append(i)
-	elif isinstance(i, int):
-		ints.append(i)
-	
+    if isinstance(i, str):
+        strs.append(i)
+    elif isinstance(i, bool):
+        bools.append(i)
+    elif isinstance(i, int):
+        ints.append(i)
+
 print(f"Characters : {strs}")
 print(f"Numbers : {ints}")
 print(f"Booleans : {bools}")
