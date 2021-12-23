1445fd2b449d19c8e70c136b990961336fe0afba
magnitopic <magnitopic@gmail.com>
2021-12-23 11:43:59 +0100

    Reworked day 12

--- Day12.py
+++ Day12.py
@@ -4,12 +4,12 @@ leters = ["A", "G", "C", "T"]
 
 def newWord():
     # Create word
-    word = [''.join(leters[np.random.choice(np.arange(0, 4), p=[.293, .207, .2, .3])]for i in range(3))for i in range(34)]
+    word = ''.join([leters[np.random.choice(np.arange(0, 4), p=[.293, .207, .2, .3])]for i in range(102)])
     print(word)
 
     # Use of ''.join() makes arrays strings of text
     for i in range(len(word)):
-        sequence = word[i]
+        sequence = word[i:i+3]
         if word.count(sequence) > 1 and len(sequence) == 3:
             print(f"{sequence} {word.count(sequence)}")
 
