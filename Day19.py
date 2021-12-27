e2e4fd1171428a15291cd403a43305bc18c7ee92
Magnitopic <magnitopic@gmail.com>
2021-12-27 23:34:04 +0100

    Start of day19

---
+++ Day19.py
@@ -0,0 +1,4 @@
+import Day9 as matrixGenerator
+# [[0, 0, -3, 0, 2], [0, -7, 0, 5, 3], [0, 0, -1, 4, -6], [2, -2, -1, 3, 0], [5, -5, 2, 1, -3]]
+# determinate = -1820
+print(matrixGenerator.main(4))
--- Day9.py
+++ Day9.py
@@ -2,7 +2,7 @@ import random
 import numpy
 
 
-def areThereCeros(array):
+def areThereCeros(array,n):
     # We loop trought all the rows
     for e in range(n):
         # Create a list of all the 0 in the row
@@ -13,23 +13,28 @@ def areThereCeros(array):
     return 0
 
 
-n = int(input("Give me the size of the cube: ") or 5)
-flag1, flag2 = True, True
-while flag1 or flag2:
-    # Creates a square with same len and height
-    array = []
-    for i in range(n):
-        row = []
-        for j in range(n):
-            # Value is randomlly chosen beteen -9 and 9, with diferent probability
-            row.append(numpy.random.choice(numpy.arange(0, 10),p=[.25, .2, .16, .12, .09, .07, .05, .03, .02, .01]))
-            # Value is has a 50/50 chance of beeing negative
-            if random.random() > .5:
-                row[j] *= -1
-        array.append(row)
-    # We cheek the first flag to see if a row is all ceros
-    flag1 = areThereCeros(array)
-    # We rotate the matrix to check the columns with the same method
-    arrayGirado=[[array[k][i] for i in range(n)for k in range(n)][i*n:(i+1)*n]for i in range(n)]
-    flag2 = areThereCeros(arrayGirado)
-print(array)
+def main(n):
+    flag1, flag2 = True, True
+    while flag1 or flag2:
+        # Creates a square with same len and height
+        array = []
+        for i in range(n):
+            row = []
+            for j in range(n):
+                # Value is randomlly chosen beteen -9 and 9, with diferent probability
+                row.append(numpy.random.choice(numpy.arange(0, 10),p=[.25, .2, .16, .12, .09, .07, .05, .03, .02, .01]))
+                # Value is has a 50/50 chance of beeing negative
+                if random.random() > .5:
+                    row[j] *= -1
+            array.append(row)
+        # We cheek the first flag to see if a row is all ceros
+        flag1 = areThereCeros(array,n)
+        # We rotate the matrix to check the columns with the same method
+        arrayGirado=[[array[k][i] for i in range(n)for k in range(n)][i*n:(i+1)*n]for i in range(n)]
+        flag2 = areThereCeros(arrayGirado,n)
+        return array
+
+
+if __name__ == "__main__":
+    n = int(input("Give me the size of the cube: ") or 5)
+    print(main(n))
