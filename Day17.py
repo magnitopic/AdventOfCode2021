56e03c07a02b73e07e3f0752775dcbecb4f1898b
magnitopic <magnitopic@gmail.com>
2021-12-27 01:56:41 +0100

    Finished day 17

--- Day17.py
+++ Day17.py
@@ -2,101 +2,117 @@ import json
 # JSON changes are not saved, have to wright to the file
 
 
+def studentExist(studentName):
+    if studentName in file:
+        return True
+    else:
+        return False
+
+
 def printMenu():
-    # Menu
     print("_"*20)
     print("Class List App")
     print(f"To interact with the information you have these options:\n\t1. Print information\n\t2. Add new information\n\t3. Edit existing information\n\t4. Delete information\n\t5. Print menu\n\t6. Exit")
-    print("_"*20)
 
 
 def newInfo():
     try:
-        changes = int(input(
-            "Would you like to...\n\t1. Add a new student\n\t2. Add new grade\nAction: "))
+        changes = int(input("Would you like to...\n\t1. Add a new student\n\t2. Add new grade\nAction: "))
     except:
         print("Invalid input. Try again.")
     else:
         if changes == 1:
             name = input("Name for the new student: ")
-            file[name] = {"average": None, "grades": []}
-        elif changes == 2:
-            name = input("Sudent's name: ")
-            try:
-                grade = float(input("New grade value: "))
-            except:
-                print("Invalid input. Try again.")
+            if not studentExist(name):
+                file[name] = {"average": None, "grades": []}
             else:
-                file[name]["grades"].append(grade)
+                print("Student already exist.")
+        elif changes == 2:
+            name = input("Student's name: ")
+            if studentExist(name):
+                try:
+                    grade = float(input("New grade value: "))
+                except:
+                    print("Invalid input. Try again.")
+                else:
+                    file[name]["grades"].append(grade)
+                    giveAverages()
+            else:
+                print("Student dosen't exist.")
         else:
             print("Invalid input. Try again.")
 
 
 def editInfo():
     try:
-        changes = int(input(
-            "Would you like to...\n\t1. Edit a student\n\t2. Edit a grade\nAction: "))
+        changes = int(input("Would you like to...\n\t1. Edit a student\n\t2. Edit a grade\nAction: "))
     except:
         print("Invalid input. Try again.")
     else:
-        if changes == 1:
-            name = input("Name of the student: ")
-            newName = input("New name for the student: ")
-            file[newName] = file.pop(name)
-        elif changes == 2:
-            name = input("Name of the student: ")
-            try:
-                position = int(input("Position of the grade: "))
-                grade = float(input("New grade value: "))
-                if position > len(file[name]["grades"]):
-                    raise ValueError('ERROR')
-            except:
-                print("Invalid input. Try again.")
+        name = input("Name of the student: ")
+        if not studentExist(name):
+            if changes == 1:
+                newName = input("New name for the student: ")
+                file[newName] = file.pop(name)
+            elif changes == 2:
+                try:
+                    position = int(input("Position of the grade: "))
+                    grade = float(input("New grade value: "))
+                    if position > len(file[name]["grades"]):
+                        raise ValueError('ERROR')
+                except:
+                    print("Invalid input. Try again.")
+                else:
+                    file[name]["grades"][position-1] = grade
+                    giveAverages()
             else:
-                file[name]["grades"][position-1] = grade
+                print("Invalid input. Try again.")
         else:
-            print("Invalid input. Try again.")
+            print("Student dosen't exist.")
 
 
 def giveAverages():
-    pass
+    for i in file.keys():
+        file[i]["average"] = round(sum(file[i]["grades"])/len(file[i]["grades"]), 2)
 
 
 def deleteInfo():
     try:
-        changes = int(input(
-            "Would you like to...\n\t1. Delete a student\n\t2. Delete a grade\n\t3. Delete everything\nAction: "))
+        changes = int(input("Would you like to...\n\t1. Delete a student\n\t2. Delete a grade\n\t3. Delete everything\nAction: "))
     except:
         print("Invalid input. Try again.")
     else:
-        if changes == 1:
-            name = input("Name of the student: ")
-            file.pop(name)
-        elif changes == 2:
-            name = input("Name of the student: ")
-            try:
-                position = int(input("Position of the grade: "))
-                if position > len(file[name]["grades"]):
-                    raise ValueError('ERROR')
-            except:
-                print("Invalid input. Try again.")
+        name = input("Name of the student: ")
+        if not studentExist(name):
+            if changes == 1:
+                file.pop(name)
+            elif changes == 2:
+                try:
+                    position = int(input("Position of the grade: "))
+                    if position > len(file[name]["grades"]):
+                        raise ValueError('ERROR')
+                except:
+                    print("Invalid input. Try again.")
+                else:
+                    del file[name]["grades"][position-1]
+                    giveAverages()
+            elif changes == 3:
+                file.clear()
             else:
-                del file[name]["grades"][position-1]
-        elif changes==3:
-            file.clear()
+                print("Invalid input. Try again.")
         else:
-            print("Invalid input. Try again.")
+            print("Student dosen't exist.")
 
 
 f = open("class.json", "r")
 text = f.read()
-
+f.close()
 file = json.loads(text)
 choice = ""
 printMenu()
-# file["class1"]={file["class1"],"Jeff": {"average": null,"grades": []}}
-# print(file)
+giveAverages()
 while choice != 6:
+    print("_"*20)
     try:
         choice = int(input("What do you what to do?: "))
         if 0 >= choice >= 6:
@@ -104,15 +120,11 @@ while choice != 6:
     except:
         print("Invalid input. Try again.")
     else:
-        if choice == 1:
-            print(json.dumps(file, indent=4, sort_keys=True))
-        if choice == 2:
-            newInfo()
-        if choice == 3:
-            editInfo()
-        if choice == 4:
-            deleteInfo()
-        if choice == 5:
-            printMenu()
+        if choice == 1:print(json.dumps(file, indent=4, sort_keys=True))
+        if choice == 2:newInfo()
+        if choice == 3:editInfo()
+        if choice == 4:deleteInfo()
+        if choice == 5:printMenu()
 print("Bye")
-f.close()
+with open('class.json', 'w') as outfile:
+    json.dump(file, outfile)
