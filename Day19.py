4f9c69d8fb2e57229ead3822760e4a600eecf555
magnitopic <magnitopic@gmail.com>
2022-01-02 13:52:24 +0100

    Shorter version of day 19

--- Day19.py
+++ Day19.py
@@ -1,24 +1,17 @@
 import random
 
-n = 3
+n = int(input("Size of the array: "))
 MainDiagonal, SecondDiagonal, array = 0, 0, []
 
-for i in range(n):
-    row = []
-    for j in range(n):
-        # Value is randomlly chosen beteen -9 and 9, with diferent probability
-        row.append(random.randint(1, 9))
-        # Value is has a 50/50 chance of beeing negative
-        if random.random() > .5:
-            row[j] *= -1
-    array.append(row)
-print(array)
-MainDiagonal += sum(array[i][i] for i in range(len(array)))
-print()
-for i in range(len(array)):
-    print(array[i][(len(array)-1)-i])
-    SecondDiagonal += array[i][(len(array)-1)-i]
+# We generate the square array
+array = [[random.randint(1, 9) for i in range(n)]for j in range(n)]
 
+# Now we figure out the two diagonals
+MainDiagonal += sum(array[i][i] for i in range(n))
+MainDiagonal += sum(array[i][(n-1)-i] for i in range(n))
+
+print(array)
 print(f"Main diagonal: {MainDiagonal}")
 print(f"Secondary diagonal: {SecondDiagonal}")
+# To get the asbsolut value of the difenence we subtract the numbers and use abs()
 print(f"Absolute value of the difference: {abs(MainDiagonal-SecondDiagonal)}")
