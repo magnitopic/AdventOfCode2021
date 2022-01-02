77140b0f5f4fc09909dceeddecfafb86518c5c83
magnitopic <magnitopic@gmail.com>
2022-01-02 13:57:03 +0100

    Small fix on day 19

--- Day19.py
+++ Day19.py
@@ -4,11 +4,12 @@ n = int(input("Size of the array: "))
 MainDiagonal, SecondDiagonal, array = 0, 0, []
 
 # We generate the square array
-array = [[random.randint(1, 9) for i in range(n)]for j in range(n)]
+array = [[random.randint(-9, 9) for i in range(n)] for j in range(n)]
 
 # Now we figure out the two diagonals
 MainDiagonal += sum(array[i][i] for i in range(n))
-MainDiagonal += sum(array[i][(n-1)-i] for i in range(n))
+SecondDiagonal += sum(array[i][(n-1)-i] for i in range(n))
+print(*(array[i][(n-1)-i] for i in range(n)))
 
 print(array)
 print(f"Main diagonal: {MainDiagonal}")
