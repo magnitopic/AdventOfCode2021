33d88eaeb60725d74058b58eac7e2efe5cc44766
magnitopic <magnitopic@gmail.com>
2022-01-06 00:21:35 +0100

    Small changes

--- Day19.py
+++ Day19.py
@@ -9,9 +9,8 @@ array = [[random.randint(-9, 9) for i in range(n)] for j in range(n)]
 # Now we figure out the two diagonals
 MainDiagonal += sum(array[i][i] for i in range(n))
 SecondDiagonal += sum(array[i][(n-1)-i] for i in range(n))
-print(*(array[i][(n-1)-i] for i in range(n)))
 
-print(array)
+print(f"Array: {array}")
 print(f"Main diagonal: {MainDiagonal}")
 print(f"Secondary diagonal: {SecondDiagonal}")
 # To get the asbsolut value of the difenence we subtract the numbers and use abs()
