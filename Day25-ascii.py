e30d9d679db21ba20e4d7ce4fab043a048e08946
magnitopic <magnitopic@gmail.com>
2022-01-14 13:15:41 +0100

    New way of showing characters

--- Day25-ascii.py
+++ Day25-ascii.py
@@ -1,25 +1,24 @@
 import random
 
-board = [[None]*3]*3
+board = [["·" for i in range(3)]for j in range(3)]
 
 
 def pritnBoard():
-    print(f"{board[0][0]}|{board[0][1]}|{board[0][2]}\n----+----+----\n{board[1][0]}|{board[1][1]}|{board[1][2]}\n----+----+----\n{board[2][0]}|{board[2][1]}|{board[2][2]}")
+    print(f" {board[0][0]} | {board[0][1]} | {board[0][2]} \n---+---+---\n {board[1][0]} | {board[1][1]} | {board[1][2]} \n---+---+---\n {board[2][0]} | {board[2][1]} | {board[2][2]} ")
 
 
 def userTurn():
     user = 1
     while user:
         print("-"*10)
-        pritnBoard()
         print()
         userChoiceX = input("What row do you what to play this turn?: ")
         userChoiceY = input("What coloumn do you what to play this turn?: ")
         try:
-            userChoiceX, userChoiceY = int(userChoiceX)-1, int(userChoiceY)-1
-            if board[userChoiceX][userChoiceY] == None:
-                board[userChoiceX][userChoiceY] = "O"
-                user = 1
+            userChoiceX, userChoiceY = int(userChoiceX), int(userChoiceY)
+            if board[userChoiceX][userChoiceY] == "·":
+                board[userChoiceX][userChoiceY] = "X"
+                user = 0
             else:
                 raise ValueError('ERROR')
         except:
@@ -27,14 +26,19 @@ def userTurn():
 
 
 def machineTurn():
-    pass
+    while True:
+        x,y=random.randint(0,2),random.randint(0,2)
+        if board[x][y]=="·":
+            board[x][y]="O"
+            break
 
-
-pritnBoard()
-userTurn()
-pritnBoard()
 
 
 def main():
+    pritnBoard()
     userTurn()
-    print()
+    machineTurn()
+    pritnBoard()
+
+
+main()
