1ea1c16af89680332ec695b9f06bb5d3c0113560
magnitopic <magnitopic@gmail.com>
2022-07-02 18:54:28 +0200

    Day 2, all versions

--- python/Day2_frankenstein-ver2.py
+++ python/Day2_frankenstein-ver2.py
@@ -1,6 +1,3 @@
 characters = [chr(i) for i in range(32, 123)]
 file = open("Frankenstein.txt", "r", encoding="utf-8").read()
 [print(f"{j} => {n}") for j in characters if (n := len([i for i in file if i == j])) != 0]
-
-# One liner:
-# [print(f"{j} => {n}") for j in [chr(i) for i in range(32, 123)] if (n := len([i for i in open("Frankenstein.txt", "r", encoding="utf-8").read() if i == j])) != 0]
